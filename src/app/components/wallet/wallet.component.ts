0952a4d64f3666e33edd88e126a867182a0222d6
raulerivassanz <Raul.ERSQ@gmail.com>
2018-12-04 18:39:07 +0800

    Add MessageService and UserInfo in wallet component

--- src/app/components/wallet/wallet.component.ts
+++ src/app/components/wallet/wallet.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { NgbActiveModal, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { MatSnackBar } from '@angular/material';
+import { SideMenuComponent } from '../side-menu/side-menu.component';
+import { NgModule } from '@angular/core';
+import { UserInfo } from '../userinfo';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.scss']
+  styleUrls: ['./wallet.component.scss'],
+  providers: [MessageService]
 })
 export class WalletComponent implements OnInit {
   selectedCoin: string;
@@ -14,9 +20,17 @@ export class WalletComponent implements OnInit {
   qrCodeAddress: string;
   sendingAmountC =0.00;
   sendingAmountU = 0.00;
-  constructor(private modalService: NgbModal) {
-    this.setCoin('BTC');
-   }
+
+  @Input() sideMenuComponent: SideMenuComponent;
+
+  constructor(
+    private modalService: NgbModal,
+    private userInfo: UserInfo,
+    private snackBar: MatSnackBar,
+    private messageService: MessageService
+  ) {
+   this.setCoin('BTC');
+  }
 
   ngOnInit() {
   }
@@ -27,7 +41,6 @@ export class WalletComponent implements OnInit {
       this.buttonColor = '#ea9914';
       this.selectedIcon = "./assets/BTC_Logo.png";
       this.selectedAddress = "14fwCjcRCNURzdQ38xfaHYgbFoQg6r6rA3";
-      
     }
     else if(coin=='ETH') {
       this.buttonColor = "#627eea";
@@ -48,7 +61,36 @@ export class WalletComponent implements OnInit {
   }
 
   openModal(content){
-    this.modalService.open(content, { centered: true });
+    if(this.userInfo.bLogined) {
+      this.modalService.open(content, { centered: true });
+    } else {
+      this.addSingle();
+    }
   }
 
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: 'top'
+    });
+  }
+
+  addSingle() {
+    this.messageService.add({ severity: 'info', summary: 'Login issue', detail: 'You have to login to use wallet.' });
+  }
+
+  addMultiple() {
+    this.messageService.addAll(
+      [
+        { severity: 'success', summary: 'Service Message', detail: 'Via MessageService' },
+        { severity: 'info', summary: 'Info Message', detail: 'Via MessageService' }
+      ]
+    );
+  }
+
+  clear() {
+    this.messageService.clear();
+  }
 }
+
+
